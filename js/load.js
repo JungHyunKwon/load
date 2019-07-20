cbfc33ebad1bdea02744985580da3b66db998056
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:14:01 +0900

    published

--- js/load.js
+++ js/load.js
@@ -2,57 +2,52 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
-	(function() {
-		'use strict';
-		
-		var _toString = Object.prototype.toString;
+(function() {
+	'use strict';
+	
+	var _toString = Object.prototype.toString;
 
-		window.load = {};
+	window.load = {};
 
-		/**
-		 * @name isArray
-		 * @since 2017-12-06
-		 * @param {*} value
-		 * @return {boolean}
-		 */
-		function _isArray(value) {
-			return _toString.call(value) === '[object Array]';
-		}
+	/**
+	 * @name isArray
+	 * @since 2017-12-06
+	 * @param {*} value
+	 * @return {boolean}
+	 */
+	function _isArray(value) {
+		return _toString.call(value) === '[object Array]';
+	}
 
-		/**
-		 * @name callback
-		 * @param {object} event
-		 * @since 2018-12-14
-		 */
-		function _callback(event) {
-			//배열일 때
-			if(_isArray(load)) {
-				for(var i = 0, loadLength = load.length; i < loadLength; i++) {
-					var element = load[i];
+	/**
+	 * @name callback
+	 * @param {object} event
+	 * @since 2018-12-14
+	 */
+	function _callback(event) {
+		//배열일 때
+		if(_isArray(load)) {
+			for(var i = 0, loadLength = load.length; i < loadLength; i++) {
+				var element = load[i];
 
-					//함수일 때
-					if(typeof element === 'function') {
-						element(event);
-					}					
-				}
-			}	
-		}
-		
-		//addEventListener가 있을 때
-		if(window.addEventListener) {
-			window.addEventListener('load', function(event) {
-				_callback(event);
-			}, false);
-		
-		//attachEvent가 있을 때
-		}else if(window.attachEvent) {
-			window.attachEvent('onload', function(event) {
-				_callback(event);
-			});
-		}
-
-	})();
-}catch(e) {
-	console.error(e);
-}
+				//함수일 때
+				if(typeof element === 'function') {
+					element(event);
+				}					
+			}
+		}	
+	}
+	
+	//addEventListener가 있을 때
+	if(window.addEventListener) {
+		window.addEventListener('load', function(event) {
+			_callback(event);
+		}, false);
+	
+	//attachEvent가 있을 때
+	}else if(window.attachEvent) {
+		window.attachEvent('onload', function(event) {
+			_callback(event);
+		});
+	}
+})();
